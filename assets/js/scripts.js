ffaabc26c7e310b7f79778ce40d0ddf90717865a
kristenkwong <kristenkwong.wy@hotmail.com>
2020-03-04 12:53:02 -0800

    CSS for mobile

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,5 @@
+var height = $(window).height();
+
 $(function() {
   var links = $("a").click(function() {
     links.removeClass("current");
@@ -7,12 +9,14 @@ $(function() {
 
 const anchor_offsets = {
   home: 0,
-  reel: $("#reel-div").offset().top,
-  contact: $("#contact-div").offset().top
+  reel: $("#reel-div").offset().top - height * 0.3,
+  contact: $("#contact-div").offset().top - height * 0.3
 };
 
 $(window).on("scroll", function() {
   var curr_offset = $(window).scrollTop();
+  console.log(curr_offset);
+  console.log("reel: " + anchor_offsets["reel"]);
 
   for (var i = 0; i < Object.keys(anchor_offsets).length; i++) {
     let key = Object.keys(anchor_offsets)[i];
