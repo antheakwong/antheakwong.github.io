3fb08cf47f753bb6b9fdd98600fe2ef2c9c2a8b2
kristenkwong <kristenkwong.wy@hotmail.com>
2020-03-28 23:49:45 -0700

    changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 var height = $(window).height();
 
-$(function() {
-  var links = $("a").click(function() {
+$(function () {
+  var links = $("a").click(function () {
     links.removeClass("current");
     $(this).addClass("current");
   });
@@ -13,17 +13,25 @@ const anchor_offsets = {
   contact: $("#contact-div").offset().top - height * 0.3
 };
 
-$(window).on("scroll", function() {
+$(window).on("scroll", function () {
   var curr_offset = $(window).scrollTop();
-  console.log(curr_offset);
-  console.log("reel: " + anchor_offsets["reel"]);
+  //console.log(curr_offset);
 
   for (var i = 0; i < Object.keys(anchor_offsets).length; i++) {
     let key = Object.keys(anchor_offsets)[i];
-    console.log(window.location.hash);
+    //console.log(window.location.hash);
     if (curr_offset >= anchor_offsets[key]) {
       $("a").removeClass("current");
       $("." + key).addClass("current");
     }
   }
+
+  if (curr_offset > (height - 50)) {
+    document.getElementById("title-nav").style.opacity = 1;
+  }
+
+  if (curr_offset < (height - 50)) {
+    document.getElementById("title-nav").style.opacity = 0;
+  }
+
 });
